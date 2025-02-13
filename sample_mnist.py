f6bd783f7b165a214feba3eb7901fab653d3f97c
Natan Hilário <natan.hilario64@gmail.com>
2025-02-12 23:43:11 -0300

    Simulation bootstrap samples geradas - Iniciar aplicação dos modelos

--- sample_mnist.py
+++ sample_mnist.py
@@ -99,7 +99,7 @@ def sample_single_bootstrap_scenario1(i_train, i_test, n_train, n_val, n_test, c
 
     sets = np.concatenate([np.repeat("train", n_train), np.repeat("val", n_val), np.repeat("test", n_test)])
     indices = np.concatenate([i_train_val, i_test])
-    pd.DataFrame({"index": indices, "set": sets}).to_csv("{}/indices_1.csv".format(directory))
+    pd.DataFrame({"index": indices, "set": sets}).to_csv("{}/indices_{}.csv".format(directory, file_index), index = False)
 
     
     # The labels for the train set are the first n_train sampled indices in i_train_val
@@ -255,13 +255,173 @@ def sample_single_bootstrap_scenario1(i_train, i_test, n_train, n_val, n_test, c
         delta_train_5bin, delta_val_5bin, delta_test_5bin
     )
     bin5_data.to_csv("{}/bin5/{}".format(directory, filename), index = False)
+
+
+def sample_single_bootstrap_scenario2(i_train, i_test, n_train, n_val, n_test, cure_probs_dict_vec, directory, file_index):
+    '''
+        Get a single bootstrap sample from the Fashion-MNIST dataset considering each distribution from scenario 1.
+    '''
+    n = n_train + n_val + n_test
+    filename = "data_{}.csv".format(file_index)
+
+    # ---------------------------- Sample the indices from the original dataset ----------------------------
     
+    # Indices for train and validation
+    i_train_val = np.random.choice(i_train, size = n_train + n_val, replace = True)
+    i_test = np.random.choice(i_test, size = n_test, replace = True)
+
+    sets = np.concatenate([np.repeat("train", n_train), np.repeat("val", n_val), np.repeat("test", n_test)])
+    indices = np.concatenate([i_train_val, i_test])
+    pd.DataFrame({"index": indices, "set": sets}).to_csv("{}/indices_{}.csv".format(directory, file_index))
+
     
+    # The labels for the train set are the first n_train sampled indices in i_train_val
+    label_train = train_labels[i_train_val[:n_train]]
+    # The labels for the validation set are the last n_train sampled indices in i_train_val
+    label_val = train_labels[i_train_val[n_train:]]
+    # Takes the labels for the test set
+    label_test = test_labels[i_test]
+    
+    p_train = cure_probs_dict_vec(label_train)
+    p_val = cure_probs_dict_vec(label_val)
+    p_test = cure_probs_dict_vec(label_test)
+
+    # The censored times follow a U(low_c, high_c) distribution - To control the censored and cured observations properly, we should have a different distribution 
+    # for each of the chosen distributions for M
+    low_c = 0
+    high_c = 6
+
+    # ---------------------------- Borel ----------------------------
+    # Borel - Training data
+    theta_train_borel = get_theta(log_a_borel, log_phi_borel, C_borel, C_inv_borel, sup_borel, p_train, theta_min = theta_min_borel, theta_max = theta_max_borel)
+    m_train_borel, t_train_borel, delta_train_borel, cured_train_borel = \
+        generate_data(log_a_borel, log_phi_borel, theta_train_borel, sup_borel, low_c, high_c)
+    # Borel - Validation data
+    theta_val_borel = get_theta(log_a_borel, log_phi_borel, C_borel, C_inv_borel, sup_borel, p_val, theta_min = theta_min_borel, theta_max = theta_max_borel)
+    m_val_borel, t_val_borel, delta_val_borel, cured_val_borel = \
+        generate_data(log_a_borel, log_phi_borel, theta_val_borel, sup_borel, low_c, high_c)
+    # Borel - Test data
+    theta_test_borel = get_theta(log_a_borel, log_phi_borel, C_borel, C_inv_borel, sup_borel, p_test, theta_min = theta_min_borel, theta_max = theta_max_borel)
+    m_test_borel, t_test_borel, delta_test_borel, cured_test_borel = \
+        generate_data(log_a_borel, log_phi_borel, theta_test_borel, sup_borel, low_c, high_c)
+    # Save the DataFrame with the simulated values for the Borel
+    borel_data = join_datasets(
+        n_train, n_val, n_test,
+        theta_train_borel, theta_val_borel, theta_test_borel,
+        m_train_borel, m_val_borel, m_test_borel,
+        t_train_borel, t_val_borel, t_test_borel,
+        delta_train_borel, delta_val_borel, delta_test_borel
+    )
+    borel_data.to_csv("{}/borel/{}".format(directory, filename), index = False)
+
+    # ---------------------------- RGP(q = 2) ----------------------------
+    # RGP(2) - Training data
+    theta_train_rgp2 = get_theta(log_a_rgp(2), log_phi_rgp(2), C_rgp(2), C_inv_rgp(2), sup_rgp(2), p_train, theta_min = theta_min_rgp, theta_max = theta_max_rgp(2))
+    m_train_rgp2, t_train_rgp2, delta_train_rgp2, cured_train_rgp2 = \
+        generate_data(log_a_rgp(2), log_phi_rgp(2), theta_train_rgp2, sup_rgp(2), low_c, high_c)
+    # RGP(2) - Validation data
+    theta_val_rgp2 = get_theta(log_a_rgp(2), log_phi_rgp(2), C_rgp(2), C_inv_rgp(2), sup_rgp(2), p_val, theta_min = theta_min_rgp, theta_max = theta_max_rgp(2))
+    m_val_rgp2, t_val_rgp2, delta_val_rgp2, cured_val_rgp2 = \
+        generate_data(log_a_rgp(2), log_phi_rgp(2), theta_val_rgp2, sup_rgp(2), low_c, high_c)
+    # RGP(2) - Test data
+    theta_test_rgp2 = get_theta(log_a_rgp(2), log_phi_rgp(2), C_rgp(2), C_inv_rgp(2), sup_rgp(2), p_test, theta_min = theta_min_rgp, theta_max = theta_max_rgp(2))
+    m_test_rgp2, t_test_rgp2, delta_test_rgp2, cured_test_rgp2 = \
+        generate_data(log_a_rgp(2), log_phi_rgp(2), theta_test_rgp2, sup_rgp(2), low_c, high_c)
+    # Save the DataFrame with the simulated values for the RGP(2)
+    rgp2_data = join_datasets(
+        n_train, n_val, n_test,
+        theta_train_rgp2, theta_val_rgp2, theta_test_rgp2,
+        m_train_rgp2, m_val_rgp2, m_test_rgp2,
+        t_train_rgp2, t_val_rgp2, t_test_rgp2,
+        delta_train_rgp2, delta_val_rgp2, delta_test_rgp2
+    )
+    rgp2_data.to_csv("{}/rgp2/{}".format(directory, filename), index = False)
+    
+
+    # ---------------------------- RGP(q = -1/10) ----------------------------
+    # RGP(-1/10) - Training data
+    theta_train_rgp10 = get_theta(log_a_rgp(-1/10), log_phi_rgp(-1/10), C_rgp(-1/10), C_inv_rgp(-1/10), sup_rgp(-1/10), p_train, theta_min = theta_min_rgp, theta_max = theta_max_rgp(-1/10))
+    m_train_rgp10, t_train_rgp10, delta_train_rgp10, cured_train_rgp10 = \
+        generate_data(log_a_rgp(-1/10), log_phi_rgp(-1/10), theta_train_rgp10, sup_rgp(-1/10), low_c, high_c)
+    # RGP(-1/10) - Validation data
+    theta_val_rgp10 = get_theta(log_a_rgp(-1/10), log_phi_rgp(-1/10), C_rgp(-1/10), C_inv_rgp(-1/10), sup_rgp(-1/10), p_val, theta_min = theta_min_rgp, theta_max = theta_max_rgp(-1/10))
+    m_val_rgp10, t_val_rgp10, delta_val_rgp10, cured_val_rgp10 = \
+        generate_data(log_a_rgp(-1/10), log_phi_rgp(-1/10), theta_val_rgp10, sup_rgp(-1/10), low_c, high_c)
+    # RGP(-1/10) - Test data
+    theta_test_rgp10 = get_theta(log_a_rgp(-1/10), log_phi_rgp(-1/10), C_rgp(-1/10), C_inv_rgp(-1/10), sup_rgp(-1/10), p_test, theta_min = theta_min_rgp, theta_max = theta_max_rgp(-1/10))
+    m_test_rgp10, t_test_rgp10, delta_test_rgp10, cured_test_rgp10 = \
+        generate_data(log_a_rgp(-1/10), log_phi_rgp(-1/10), theta_test_rgp10, sup_rgp(-1/10), low_c, high_c)
+    # Save the DataFrame with the simulated values for the RGP(-1/10)
+    rgp10_data = join_datasets(
+        n_train, n_val, n_test,
+        theta_train_rgp10, theta_val_rgp10, theta_test_rgp10,
+        m_train_rgp10, m_val_rgp10, m_test_rgp10,
+        t_train_rgp10, t_val_rgp10, t_test_rgp10,
+        delta_train_rgp10, delta_val_rgp10, delta_test_rgp10
+    )
+    rgp10_data.to_csv("{}/rgp10/{}".format(directory, filename), index = False)
+
+    # ---------------------------- Haight ----------------------------
+    # Haight - Training data
+    theta_train_haight = get_theta(log_a_haight, log_phi_haight, C_haight, C_inv_haight, sup_haight, p_train, theta_min = theta_min_haight, theta_max = theta_max_haight)
+    m_train_haight, t_train_haight, delta_train_haight, cured_train_haight = \
+        generate_data(log_a_haight, log_phi_haight, theta_train_haight, sup_haight, low_c, high_c)
+    # Haight - Validation data
+    theta_val_haight = get_theta(log_a_haight, log_phi_haight, C_haight, C_inv_haight, sup_haight, p_val, theta_min = theta_min_haight, theta_max = theta_max_haight)
+    m_val_haight, t_val_haight, delta_val_haight, cured_val_haight = \
+        generate_data(log_a_haight, log_phi_haight, theta_val_haight, sup_haight, low_c, high_c)
+    # Haight - Test data
+    theta_test_haight = get_theta(log_a_haight, log_phi_haight, C_haight, C_inv_haight, sup_haight, p_test, theta_min = theta_min_haight, theta_max = theta_max_haight)
+    m_test_haight, t_test_haight, delta_test_haight, cured_test_haight = \
+        generate_data(log_a_haight, log_phi_haight, theta_test_haight, sup_haight, low_c, high_c)
+    # Save the DataFrame with the simulated values for the Haight
+    haight_data = join_datasets(
+        n_train, n_val, n_test,
+        theta_train_haight, theta_val_haight, theta_test_haight,
+        m_train_haight, m_val_haight, m_test_haight,
+        t_train_haight, t_val_haight, t_test_haight,
+        delta_train_haight, delta_val_haight, delta_test_haight
+    )
+    haight_data.to_csv("{}/haight/{}".format(directory, filename), index = False)
+
+    # ---------------------------- Geeta(q = 3) ----------------------------
+    # Geeta(3) - Training data
+    theta_train_geeta3 = get_theta(log_a_geeta(3), log_phi_geeta(3), C_geeta(3), C_inv_geeta(3), sup_geeta, p_train, theta_min = theta_min_rgp, theta_max = theta_max_geeta(3))
+    m_train_geeta3, t_train_geeta3, delta_train_geeta3, cured_train_geeta3 = \
+        generate_data(log_a_geeta(3), log_phi_geeta(3), theta_train_geeta3, sup_geeta, low_c, high_c)
+    # Geeta(3) - Validation data
+    theta_val_geeta3 = get_theta(log_a_geeta(3), log_phi_geeta(3), C_geeta(3), C_inv_geeta(3), sup_geeta, p_val, theta_min = theta_min_rgp, theta_max = theta_max_geeta(3))
+    m_val_geeta3, t_val_geeta3, delta_val_geeta3, cured_val_geeta3 = \
+        generate_data(log_a_geeta(3), log_phi_geeta(3), theta_val_geeta3, sup_geeta, low_c, high_c)
+    # Geeta(3) - Test data
+    theta_test_geeta3 = get_theta(log_a_geeta(3), log_phi_geeta(3), C_geeta(3), C_inv_geeta(3), sup_geeta, p_test, theta_min = theta_min_rgp, theta_max = theta_max_geeta(3))
+    m_test_geeta3, t_test_geeta3, delta_test_geeta3, cured_test_geeta3 = \
+        generate_data(log_a_geeta(3), log_phi_geeta(3), theta_test_geeta3, sup_geeta, low_c, high_c)
+    # Save the DataFrame with the simulated values for the Geeta(3)
+    geeta3_data = join_datasets(
+        n_train, n_val, n_test,
+        theta_train_geeta3, theta_val_geeta3, theta_test_geeta3,
+        m_train_geeta3, m_val_geeta3, m_test_geeta3,
+        t_train_geeta3, t_val_geeta3, t_test_geeta3,
+        delta_train_geeta3, delta_val_geeta3, delta_test_geeta3
+    )
+    geeta3_data.to_csv("{}/geeta3/{}".format(directory, filename), index = False)
 
     
 if(__name__ == "__main__"):
-    # Loading dataset
 
+    deleted_directory = False
+    
+    print("Do you want to delete all the directories before starting the generation?")
+    r = input()
+    if(r[0] == "y"):
+        shutil.rmtree("SimulationDataset")
+        deleted_directory = True
+    
+    np.random.seed(111)
+    
+    print("------------------------------------------------------------------------------------")
+    
     print("Loading Fashion-MNIST dataset...")
     fashion_mnist = tf.keras.datasets.fashion_mnist
     (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
@@ -279,46 +439,75 @@ if(__name__ == "__main__"):
 
     print("Dimension of the train set: {}".format(train_images.shape))
     print("Dimension of the test set: {}".format(test_images.shape))
-    print("------------------------------------------------------------------------")
+    print("------------------------------------------------------------------------------------")
     
     print("Creating directories structure")
     dists_scenario1 = ["poisson", "logarithmic", "geometric", "nb2", "bernoulli", "bin5"]
-    dists_scenario2 = ["borel", "rgp2", "rgp10", "haight", "geeta"]
+    dists_scenario2 = ["borel", "rgp2", "rgp10", "haight", "geeta3"]
     for dist in dists_scenario1:
-        Path("Datasets/Scenario1/n500/{}".format(dist)).mkdir(parents=True, exist_ok=True)
-        Path("Datasets/Scenario1/n1000/{}".format(dist)).mkdir(parents=True, exist_ok=True)
-        Path("Datasets/Scenario1/n3000/{}".format(dist)).mkdir(parents=True, exist_ok=True)
+        Path("SimulationDataset/Scenario1/n500/{}".format(dist)).mkdir(parents=True, exist_ok=True)
+        Path("SimulationDataset/Scenario1/n1000/{}".format(dist)).mkdir(parents=True, exist_ok=True)
+        Path("SimulationDataset/Scenario1/n3000/{}".format(dist)).mkdir(parents=True, exist_ok=True)
     for dist in dists_scenario2:
-        Path("Datasets/Scenario2/n500/{}".format(dist)).mkdir(parents=True, exist_ok=True)
-        Path("Datasets/Scenario2/n1000/{}".format(dist)).mkdir(parents=True, exist_ok=True)
-        Path("Datasets/Scenario2/n3000/{}".format(dist)).mkdir(parents=True, exist_ok=True)
+        Path("SimulationDataset/Scenario2/n500/{}".format(dist)).mkdir(parents=True, exist_ok=True)
+        Path("SimulationDataset/Scenario2/n1000/{}".format(dist)).mkdir(parents=True, exist_ok=True)
+        Path("SimulationDataset/Scenario2/n3000/{}".format(dist)).mkdir(parents=True, exist_ok=True)
 
-    print("------------------------------------------------------------------------")
-
-
-
-    # <td> 1 </td> <td> 500 </td> <td> 716 </td> <td> 108 </td> <td> 108 </td>
-    # </tr>
-    # <tr>
-    #     <td> 2 </td> <td> 1000 </td> <td> 1428 </td> <td> 214 </td> <td> 214 </td>
-    # </tr>
-    # <tr>
-    #     <td> 3 </td> <td> 3000 </td> <td> 4286 </td> <td> 643 </td> <td> 643 </td>
-
+    if(not deleted_directory):
+        print("Do you want to run scenario 1? It may delete files that already exist!")
+        r = input()
+    # If the directory was deleted, it's better to just run everything without having to ask the user
+    if(deleted_directory or r[0] == "y"):
+        print("---------------------------- Scenario 1 ----------------------------")
+        cure_probs_dict1 = {0: 0.9, 1:0.45, 2:0.22, 3:0.14, 4: 0.08}
+        cure_probs_dict1 = np.vectorize(cure_probs_dict1.get)
     
-    print("Taking a sample for each of the distributions of interest")
-    cure_probs_dict1 = {0: 0.9, 1:0.45, 2:0.22, 3:0.14, 4: 0.08}
-    cure_probs_dict1 = np.vectorize(cure_probs_dict1.get)
+        train_sizes = [500, 1000, 3000] # 3 sample sizes
+        val_sizes = [108, 214, 643]
+        test_sizes = [108, 214, 643]
     
-    # sample_single_bootstrap_scenario1(i_train, i_test, n_train, n_val, n_test, cure_probs_dict, directory, file_index)
-    sample_single_bootstrap_scenario1(i_train, i_test, n_train = 500, n_val = 108, n_test = 108, cure_probs_dict_vec = cure_probs_dict1, directory = "Datasets/Scenario1/n500", file_index = 1)
+        n_replicates = 100
+        
+        for j in range(len(train_sizes)):
+            n_train = train_sizes[j]
+            n_val = val_sizes[j]
+            n_test = test_sizes[j]
+            print("n = {}".format(n_train))
+            for i in tqdm(range(n_replicates)):
+                sample_single_bootstrap_scenario1(
+                    i_train,
+                    i_test,
+                    n_train, n_val, n_test,
+                    cure_probs_dict_vec = cure_probs_dict1,
+                    directory = "SimulationDataset/Scenario1/n{}".format(n_train), file_index = i+1
+                )
 
+    np.random.seed(222)
 
-
-
-
-
-
-
-
+    if(not deleted_directory):
+        print("Do you want to run scenario 2? It may delete files that already exist!")
+        r = input()
+    if(deleted_directory or r[0] == "y"):
+        print("---------------------------- Scenario 2 ----------------------------")
+        cure_probs_dict2 = {0: 0.95, 1:0.85, 2:0.75, 3:0.65, 4: 0.55}
+        cure_probs_dict2 = np.vectorize(cure_probs_dict2.get)
     
+        train_sizes = [500, 1000, 3000] # 3 sample sizes
+        val_sizes = [108, 214, 643]
+        test_sizes = [108, 214, 643]
+    
+        n_replicates = 100
+        
+        for j in range(len(train_sizes)):
+            n_train = train_sizes[j]
+            n_val = val_sizes[j]
+            n_test = test_sizes[j]
+            print("n = {}".format(n_train))
+            for i in tqdm(range(n_replicates)):
+                sample_single_bootstrap_scenario2(
+                    i_train,
+                    i_test,
+                    n_train, n_val, n_test,
+                    cure_probs_dict_vec = cure_probs_dict2,
+                    directory = "SimulationDataset/Scenario2/n{}".format(n_train), file_index = i+1
+                )
